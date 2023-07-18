533c4986a45bbfb051884339e03c69a123664d71
humphreynjuguna <humphreynjuguna98@gmail.com>
2023-07-19 00:12:01 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+
+function upperCaseCustomerName(){
+    if (customerName){
+        customerName = customerName.toUpperCase();
+    }
+}
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+}
+
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+const leastFavouriteCustomer = 'Humphrey';
+
+function changeLeastFavoriteCustomer () {
+    leastFavouriteCustomer = 'Mercy';
+}
+
+console.log(customerName)
